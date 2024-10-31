6fb1a4eeef287320129c8276e66b0120e34e11be
Dante LoPriore <131166199+Dlopriore@users.noreply.github.com>
2024-10-31 14:00:15 -0400

    Update bare_logic.py
    This version is the initial start for the flight reservation database
    Updated Bare Logic Component:
    - Included a Flight Database Object that stores all Flights, which each flight has its own attributes.
    - Created a BFS Algorithm for indirect flights
    - Created a menu to interact with selecting flights and reserving seats separately. 
    - Sorting by Location, Duration.
    
    TASKS NEEDED TO BE COMPLETED:
    - Consider Time Intervals when sorting like fitting into a schedule 
    - Include type of seats
    - Consider multiple days.

--- bare_logic.py
+++ bare_logic.py
@@ -1,7 +1,10 @@
+from collections import deque
 
 class SeatNode:
-    def __init__(self, data=0):
+    def __init__(self, data=0, cost=100, status= "Economy"):
         self.data = data
+        self.status = status
+        self.cost = cost
         self.next = None
 
 class SingleLinkedSeatingList:
@@ -151,54 +154,266 @@ class SingleLinkedSeatingList:
             temp = temp.next
 
         print()  # Print a newline at the end
+
+class Flight(SingleLinkedSeatingList):
+    def __init__(self, flightNumber, departureLocation, arrivalLocation, timeInterval, amountSeats=30, duration=30):
+        super().__init__()
+        self.flightNumber = flightNumber
+        self.departureLocation = departureLocation
+        self.arrivalLocation = arrivalLocation
+
+        aval_seating_list = SingleLinkedSeatingList()
+        booked_seating_list = SingleLinkedSeatingList()
+
+        # Add seats to the available seats in the linked list
+        for i in range(amountSeats):
+            if(i % 2 == 0):
+                seatNumber = (i // 2 + 1) * 100 + 1
+            else:
+                seatNumber = (i // 2 + 1) * 100 + 2            
+            aval_seating_list.insert_at_end(seatNumber)
+
+        self.aval_seating_list  = aval_seating_list
+        self.booked_seating_list = booked_seating_list
+        self.timeInterval =timeInterval
+        self.duration = duration
     
-def main_menu_catalog():
-    # Initialize variables to store the single linked lists for available and booked seating
-    aval_seating_list = SingleLinkedSeatingList()
-    booked_seating_list = SingleLinkedSeatingList()
-    max_seat_in_row = 12
+    def display_flight(self):
+        print(f'Flight ID Number: {self.flightNumber}, Departure Location: {self.departureLocation}, Arrival Location: {self.arrivalLocation}, Time Interval: {self.timeInterval}, Time Duration: {self.duration}')
 
-    # Add seats to the available seats in the linked list
-    for i in range(max_seat_in_row):
-        aval_seating_list.insert_at_end(100 + i)
+    def main_menu_catalog_seating(self):
+        # Initialize variables to store the single linked lists for available and booked seating
+        # aval_seating_list = SingleLinkedSeatingList()
+        # booked_seating_list = SingleLinkedSeatingList()
+        if self is None:
+            print("Null Point")
+            return 
+        
+        while True:
+            # Create Menu UI options for user to interact with
+            print("Flight Reservation Options for Flight: " + str(self.flightNumber))
+            print("Departure: " + self.departureLocation)
+            print("Arrival: " + self.arrivalLocation)
+            print("Time Duration: " + str(self.duration))
+            print("")
+            print("Event Ticket Booking System: MAIN MENU")
+            print("Please Select an Option:")
+            print("1. Book a Seat")
+            print("2. Cancel a Seat")
+            print("3. Show Available Seats (All Booked Seats List)")
+            print("4. Exit")
+            print("5. Show All Open Seats")
 
-    while True:
-        # Create Menu UI options for user to interact with
-        print("Event Ticket Booking System: MAIN MENU")
-        print("Please Select an Option:")
-        print("1. Book a Seat")
-        print("2. Cancel a Seat")
-        print("3. Show Available Seats (All Booked Seats List)")
-        print("4. Exit")
-        print("5. Show All Open Seats")
+            user_input = input("Insert Operation Number: ")
+            print()
 
-        user_input = input("Insert Operation Number: ")
-        print()
+            match user_input:
+                case "1":
+                    print("\nYou selected to Book a Seat")
+                    self.booked_seating_list.book_seat(self.aval_seating_list)
+                    print()
+                case "2":
+                    print("\nYou selected to Cancel a Seat")
+                    self.booked_seating_list.cancel_a_seat(self.aval_seating_list)
+                    print()
+                case "3":
+                    print("\nYou selected Display Available Seats. (Booked Seats)")
+                    self.booked_seating_list.to_print()
+                    print()
+                case "4":
+                    print("\nYou selected to terminate the program. Exiting...")
+                    print()
+                    exit()
+                case "5":
+                    print("\nYou selected Display All Open Seats")
+                    self.aval_seating_list.to_print()
+                    print()
+                case _:
+                    print("\nYou selected an invalid option. Please reselect your input")
+                    print()
 
-        match user_input:
-            case "1":
-                print("\nYou selected to Book a Seat")
-                booked_seating_list.book_seat(aval_seating_list)
-                print()
-            case "2":
-                print("\nYou selected to Cancel a Seat")
-                booked_seating_list.cancel_a_seat(aval_seating_list)
-                print()
-            case "3":
-                print("\nYou selected Display Available Seats. (Booked Seats)")
-                booked_seating_list.to_print()
-                print()
-            case "4":
-                print("\nYou selected to terminate the program. Exiting...")
-                print()
-                exit()
-            case "5":
-                print("\nYou selected Display All Open Seats")
-                aval_seating_list.to_print()
-                print()
-            case _:
-                print("\nYou selected an invalid option. Please reselect your input")
+    
+
+class FlightDatabase():
+    
+    def __init__(self):
+        self.givenFlights = []
+
+    def add_flight(self, givenFlight):
+        self.givenFlights.append(givenFlight)
+    
+    def get_flight(self, userInput):
+        index = 0
+        for index in range(len(self.givenFlights)):
+            if(int(userInput) == self.givenFlights[index].flightNumber):
+                return index
+        return -1
+    
+    def display_flight(self):
+
+        if not self.givenFlights:
+            return "Throw ERROR: NO Flights Exist"
+        else:
+            for currentFlight in self.givenFlights:
+                print(f'Flight ID Number: {currentFlight.flightNumber}, Departure Location: {currentFlight.departureLocation}, Arrival Location: {currentFlight.arrivalLocation}, Time Interval: {currentFlight.timeInterval}, Time Duration: {currentFlight.duration}')
+    
+    def checkFlightExistsBasedOnFlightId(self, userInput):
+        for curFlight in self.givenFlights:
+            if(int(userInput) == int(curFlight.flightNumber)):
+                return True
+        return False
+    
+    def flightExists(self, fromSpace, toSpace):
+        for curFlight in self.givenFlights:
+            if(fromSpace == curFlight.departureLocation and toSpace == curFlight.arrivalLocation):
+                return True
+        return False
+    
+    def getFlightNumber(self, fromSpace, toSpace):
+        for curFlight in self.givenFlights:
+            if(fromSpace == curFlight.departureLocation and toSpace == curFlight.arrivalLocation):
+                return curFlight.flightNumber
+        return False
+    
+    def sortbyDuration(self):
+        index = 0
+        jindex = 0
+        for index in range(len(self.givenFlights)):
+            for jindex in range(len(self.givenFlights)):
+                if(self.givenFlights[index].duration < self.givenFlights[jindex].duration):
+                   temp = self.givenFlights[index]
+                   temp2 = self.givenFlights[jindex]
+                   self.givenFlights[jindex] =  temp
+                   self.givenFlights[index] =  temp2
+        
+        for curFight in self.givenFlights:
+            curFight.display_flight()
+
+    def sortbyNonOverlappingActivities(self):
+        nonOverlapSced = []
+
+
+        sortedFlights = sorted(self.givenFlights, key=lambda flight: flight.timeInterval[1])
+        sizeOfFlightLog = len(self.givenFlights)
+
+        lastKnownEndTime = -1
+        index=1 
+
+        dpArray = [1] * sizeOfFlightLog
+        previousIndex = [-1] *sizeOfFlightLog
+
+        for index in range(1, sizeOfFlightLog):
+            for jindex in range(index):
+
+                curActivityStartTime = self.givenFlights[jindex].timeInterval[0]
+                if (self.givenFlights[index].timeInterval[1] <= curActivityStartTime):
+                    if (dpArray[index] < dpArray[jindex]+1):
+                        dpArray[index] = dpArray[jindex]+1
+                        previousIndex[index] = jindex
+        
+        maxIndex = dpArray.index(max(dpArray))
+        while -1 != maxIndex:
+            nonOverlapSced.append(sortedFlights[maxIndex])
+            maxIndex = previousIndex[maxIndex]
+        
+        for flight in nonOverlapSced:
+            flight.display_flight()
+
+
+    #Use BFS
+    def find_indirect_flights(self, source, dest):
+        known_indirect_flights = []
+        visitedSet = set()  # To keep track of visited nodes
+
+        departure_map = {}
+        for flight in self.givenFlights:
+            if flight.departureLocation not in departure_map:
+                departure_map[flight.departureLocation] = []
+            departure_map[flight.departureLocation].append(flight)
+        
+        queue = deque([(source, [])])
+
+        while (queue):
+            currentFlight, flightPath = queue.popleft()
+
+            if currentFlight not in visitedSet:
+                visitedSet.add(currentFlight)
+            
+                if currentFlight in departure_map:
+                    for flight in departure_map[currentFlight]:
+                        curpath = flightPath + [flight.flightNumber]
+
+                        if flight.arrivalLocation == dest:
+                            known_indirect_flights.append(curpath)
+
+                        if flight.arrivalLocation not in visitedSet:
+                            queue.append((flight.arrivalLocation, curpath))
+
+
+        return known_indirect_flights
+    
+    def display_indirect_flights(self, sourceLocation, destLocation):
+        all_known_indirect_flights = self.find_indirect_flights(sourceLocation, destLocation)
+
+        count = 1
+
+        if all_known_indirect_flights is not None:
+            for curOverallFlightPath in all_known_indirect_flights:
+                if(len(curOverallFlightPath) > 1):
+                    print("Indirect Flight Option: " + str(count))
+                    count+=1
+                for curIndirectFlight in curOverallFlightPath:
+                    if(len(curOverallFlightPath) > 1):
+                        self.givenFlights[self.get_flight(curIndirectFlight)].display_flight()
+                
                 print()
 
-while True:
-    main_menu_catalog()
+    def display_direct_flights(self, sourceLocation, destLocation):
+        count = 1
+
+        if self.givenFlights is not None:
+            for cur_flight in self.givenFlights:
+                
+                
+                if((cur_flight.departureLocation == sourceLocation) and (cur_flight.arrivalLocation == destLocation)):
+                    print("Direct Flight Option: " + str(count))
+                    cur_flight.display_flight()
+                    print()
+        
+        
+    def select_flight_to_book(self):
+        print("Available Flight List")
+        self.display_flight()
+
+
+        fromDestination = input("Please Select a Flight Depature: " )
+        toDestination = input("Please Select a Flight Arrival: " )
+        index = input("1 to show avaliability or 2 to book the flight: ")
+
+        if(index == 1):
+            self.display_direct_flights(fromDestination, toDestination)
+            self.display_indirect_flights(fromDestination, toDestination)
+        if(index == 3):
+            flightdata.sortbyDuration()
+        else:
+            if(self.flightExists(fromDestination, toDestination)):
+
+                flight = self.getFlightNumber(fromDestination, toDestination)
+                self.givenFlights[self.get_flight(flight)].main_menu_catalog_seating()
+            else:
+                print("False")
+    
+
+if __name__ == "__main__":
+    flightdata = FlightDatabase()
+    flightdata.add_flight(Flight(101, "BOS", "CEE", (3,4), 12,  30))
+    flightdata.add_flight(Flight(102, "ALT", "BOS", (5,9), 12,  90))
+    flightdata.add_flight(Flight(103, "MIA", "LAX", (10,12), 12,  123))
+    flightdata.add_flight(Flight(104, "LAX", "ALT", (1,6), 12,  677))
+    flightdata.add_flight(Flight(105, "ALT", "LAX", (8,12), 12,  908))
+    flightdata.add_flight(Flight(106, "BOS", "ALT", (3,6), 12,  67))
+    flightdata.add_flight(Flight(107, "CEE", "MIA", (3,4), 12,  350))
+    flightdata.add_flight(Flight(108, "CEE", "LAX", (2,5), 12,  234))
+    
+    print(flightdata.select_flight_to_book())
+   
